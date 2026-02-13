09680569268ed47d6e473eff9ff6d167f8f7eae2
Jarvis <18375166@buaa.edu.cn>
2026-02-13 08:49:15 +0000

    20260213 run

--- Scripts/downstream.sh
+++ Scripts/downstream.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-n="SG_llama2_tale_256_Epoch_5"
-pn='tale_256_sg'
+embed_name="SG_llama2_tale_256_Epoch_5"
+baseline_name='tale_256_sg'
 dataset='SG'
 
 echo $n
 
-# python Downstream/poi_clf.py    --gpu 3 --NAME $n --dataset $dataset --POI_MODEL_NAME $pn
+python Downstream/poi_clf.py    --gpu 3 --NAME  $embed_name --dataset $dataset --POI_MODEL_NAME $baseline_name
 
-# echo $n
+echo $embed_name
 
-# echo "----------- task 1 done -----------"
+echo "----------- task 1 done -----------"
 
 
 
-python Downstream/traj_next_pre.py    --gpu 3 --NAME $n --dataset $dataset --POI_MODEL_NAME $pn
+python Downstream/traj_next_pre.py    --gpu 3 --NAME $embed_name --dataset $dataset --POI_MODEL_NAME $baseline_name
 
-echo $n
+echo $embed_name
 
 echo "----------- task 2 done -----------"
+
+python Downstream/poi_cluster.py    --gpu 3 --NAME $embed_name --dataset $dataset --POI_MODEL_NAME $baseline_name
+
+echo $embed_name
+
+echo "----------- task 3 done -----------"
 
--- Scripts/llm_embed_extract.sh
+++ Scripts/llm_embed_extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for variable1 in 1
+for variable1 in 'NY' 'SG' 'TKY'
     do
     for variable2 in   'address' 'time' 'cat_nearby'
     do  
-        python Tools/get_embedding_from_LLM.py  --LLM llama2 --dataset TKY --gpu 3 --prompt_type $variable2
+        python Tools/get_embedding_from_LLM.py  --LLM llama2 --dataset $variable1 --gpu 3 --prompt_type $variable2
     done
 done
 
